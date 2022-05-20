c26c893f3512f028d27fbb8faa275c2fd2e5de65
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-05-20 15:45:34 +0300

    commit

--- server.js
+++ server.js
@@ -173,10 +173,6 @@ router.get('logout', (req, res) => {
 //add the router
 app.use('/', router);
 
-
-//process.env.port we use when there is environment variables file
-//and when there is not then we use 3000 port
-//currently we dis not learn it but I love to be like pro
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
